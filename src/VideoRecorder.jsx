b629e76593dedf9e70a39e00aad1f7829bd8e822
s-techlead <sebastienschnd.techlead@gmail.com>
2023-10-18 21:33:01 +0200

    VideoRecorder and audiorecorder ending

--- src/VideoRecorder.jsx
+++ src/VideoRecorder.jsx
@@ -6,7 +6,7 @@ const VideoRecorder = () => {
     const [permission, setPermission] = useState(false);
     const mediaRecorder = useRef(null);
     const liveVideoFeed = useRef(null);
-    const [recordingStats, setRecordingStatus] = useState("inactive");
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
     const [stream, setStream] = useState(null);
     const [videoChunks, setVideoChunks] = useState([]);
     const [recordedVideo, setRecordedVideo] = useState(null);
@@ -60,26 +60,58 @@ const VideoRecorder = () => {
         setVideoChunks(localVideoChunks);
     };
 
+    const stopRecording = () => {
+        setPermission(false);
+        setRecordingStatus("inactive");
+        mediaRecorder.current.stop();
+        mediaRecorder.current.onstop = () => {
+            const videoBlob = new Blob(videoChunks, { type: mimeType });
+            const videoUrl = URL.createObjectURL(videoBlob);
+            setRecordedVideo(videoUrl);
+            setVideoChunks([]);
+        }
+    }
+
 
     return (
-        <div>
-            <h2>Video Recorder</h2>
-            <main>
-                <div className="video-controls">
-                    {!permission ? (
-                        <button onClick={getCameraPermission} type="button">
-                            Get Camera
-                        </button>
-                    ):null}
-                    {permission ? (
-                        <button type="button">
-                            Record
-                        </button>
-                    ):null}
-                </div>
-            </main>
-        </div>
-    )
-}
+		<div>
+			<h2>Video Recorder</h2>
+			<main>
+				<div className="video-controls">
+					{!permission ? (
+						<button onClick={getCameraPermission} type="button">
+							Get Camera
+						</button>
+					) : null}
+					{permission && recordingStatus === "inactive" ? (
+						<button onClick={startRecording} type="button">
+							Start Recording
+						</button>
+					) : null}
+					{recordingStatus === "recording" ? (
+						<button onClick={stopRecording} type="button">
+							Stop Recording
+						</button>
+					) : null}
+				</div>
+			</main>
+
+			<div className="video-player">
+				{!recordedVideo ? (
+					<video ref={liveVideoFeed} autoPlay className="live-player"></video>
+				) : null}
+				{recordedVideo ? (
+					<div className="recorded-player">
+						<video className="recorded" src={recordedVideo} controls></video>
+						<a download href={recordedVideo}>
+							Download Recording
+						</a>
+					</div>
+				) : null}
+			</div>
+		</div>
+	);
+};
+
 
 export default VideoRecorder;
